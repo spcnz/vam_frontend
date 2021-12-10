7f3c7d5b0879b891277ce3be3eaf9eda3872e887
specnazm <laketic.milena98@gmail.om>
2021-12-10 21:01:07 +0100

    Moved calcTotal to utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,8 @@
 export const round = price => {
     return typeof(price) === 'number'? price.toFixed(2) : price;
 }
+
+export const calcTotal = products => {
+    return products.reduce((res,item) => res + item.price * item.quantity ,0)
+}
+  
